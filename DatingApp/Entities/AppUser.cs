7657dbb30f31f8245403680517334f3d8993cda0
Katarina Milosavcevic <milosavcevic.katarinaa@gmail.com>
2026-03-03 19:35:23 +0100

    Fix DisplayName column typo

--- DatingApp/Entities/AppUser.cs
+++ DatingApp/Entities/AppUser.cs
@@ -5,7 +5,7 @@ namespace DatingApp.Entities
     public class AppUser
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
-        public required string DispalyName { get; set; }
+        public required string DisplayName { get; set; }
         public required string Email { get; set; }
     }
 }
